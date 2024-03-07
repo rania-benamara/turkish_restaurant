13f1eea57c33dcd01ec96dee4d1ae3a3b4582c60
Rania Benamara <raniabenamara1101@gmail.com>
2024-03-07 12:11:14 -0500

    responsive version 1

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+function showSidebar(){
+  const sidebar = document.querySelector('.navLinks')
+  sidebar.style.display = 'flex'
+}
+function hideSidebar(){
+  const sidebar = document.querySelector('.navLinks')
+  sidebar.style.display = 'none'
+}
