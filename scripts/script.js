2fa555bfb15fae6fc13c4354e5fecbeb412d1b5a
Rania Benamara <raniabenamara1101@gmail.com>
2024-03-08 11:51:19 -0500

    interactive

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,23 @@ function hideSidebar(){
   const sidebar = document.querySelector('.navLinks')
   sidebar.style.display = 'none'
 }
+//-----------------------------------------------------
+const menu=document.querySelectorAll('.tab-item');
+
+const menuId=document.querySelectorAll('.menu-content');
+function selectItem(e){
+
+  removeShow();
+  const menuItem = document.querySelector(`#${this.id}-content`);  
+  menuItem.classList.add('show');
+
+}
+
+function removeShow(){
+   menu.forEach(item =>{
+    item.classList.remove('show');
+   });
+}
+menuId.forEach(item => {
+  item.addEventListener('click',selectItem);
+});
